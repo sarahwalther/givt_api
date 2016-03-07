931273f42cb5a6568b9454e1552bcf249beb76e6
kevinmcalear <kevin.mcalear@gmail.com>
2016-03-07 08:40:11 -0800

    updates basic menu item behavior + tests

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,6 @@
 class MenuItemsController < ApplicationController
   before_action :set_menu_item, only: [:show, :update, :destroy]
+  before_action :authenticate
 
   # GET /menu_items
   def index
--- test/controllers/menu_items_controller_test.rb
+++ test/controllers/menu_items_controller_test.rb
@@ -3,35 +3,36 @@ require 'test_helper'
 class MenuItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @restaurant = restaurants(:one)
+    @manager = users(:manager)
     @menu_item = menu_items(:one)
   end
 
   test "should get index" do
-    get restaurant_menu_items_url(@restaurant)
+    get restaurant_menu_items_url(@restaurant), headers: api_key(@manager)
     assert_response :success
   end
 
   test "should create menu_item" do
     assert_difference('MenuItem.count') do
-      post restaurant_menu_items_url(@restaurant), params: { menu_item: { description: @menu_item.description, image_url: @menu_item.image_url, name: @menu_item.name, price: @menu_item.price, restaurant_id: @menu_item.restaurant_id } }
+      post restaurant_menu_items_url(@restaurant), headers: api_key(@manager), params: { menu_item: { description: @menu_item.description, image_url: @menu_item.image_url, name: @menu_item.name, price: @menu_item.price, restaurant_id: @menu_item.restaurant_id } }
     end
 
     assert_response 201
   end
 
   test "should show menu_item" do
-    get restaurant_menu_item_url(@restaurant, @menu_item)
+    get restaurant_menu_item_url(@restaurant, @menu_item), headers: api_key(@manager)
     assert_response :success
   end
 
   test "should update menu_item" do
-    patch restaurant_menu_item_url(@restaurant, @menu_item), params: { menu_item: { description: @menu_item.description, image_url: @menu_item.image_url, name: @menu_item.name, price: @menu_item.price, restaurant_id: @menu_item.restaurant_id } }
+    patch restaurant_menu_item_url(@restaurant, @menu_item), headers: api_key(@manager), params: { menu_item: { description: @menu_item.description, image_url: @menu_item.image_url, name: @menu_item.name, price: @menu_item.price, restaurant_id: @menu_item.restaurant_id } }
     assert_response 200
   end
 
   test "should destroy menu_item" do
     assert_difference('MenuItem.count', -1) do
-      delete restaurant_menu_item_url(@restaurant, @menu_item)
+      delete restaurant_menu_item_url(@restaurant, @menu_item), headers: api_key(@manager)
     end
 
     assert_response 204
